6740401e5dddc594d55dee948c586dd6908a5d67
devisabriani <40249362+devisabriani@users.noreply.github.com>
2024-09-06 22:46:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from crewai import Agent, Task, Crew
+#from crewai import Agent, Task, Crew
 def main():
     st.set_page_config(page_title="EduCrew - by Devis Abriani")
     st.title("EduCrew")
