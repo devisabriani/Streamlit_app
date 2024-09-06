cab4557881c489b7d3235cf45219a57eba5ce569
devisabriani <40249362+devisabriani@users.noreply.github.com>
2024-09-06 22:34:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 import streamlit as st
-st.title("EduCrew")
+def main():
+    st.set_page_config(page_title="EduCrew - by Devis Abriani")
+    st.title("EduCrew")
+    st.markdown("*by Devis Abriani*")
+
+if __name__ == "__main__":
+    main()
