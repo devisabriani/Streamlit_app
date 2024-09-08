bce88dd53cde0fb32ef61d513090913f9da7ed26
devisabriani <40249362+devisabriani@users.noreply.github.com>
2024-09-08 23:13:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,16 +10,13 @@ st.markdown("*by Devis Abriani*")
 classe = st.text_input("Inserisci la classe:")
 argomento = st.text_input("Inserisci l'argomento della lezione:")
 
-openai_api_key = st.secrets["OPENAI_API_KEY"]
-#openai.api_key = st.secrets["openai_api_key"]
-#openai_api_key = os.getenv('OPENAI_API_KEY')
+#openai_api_key = st.secrets["OPENAI_API_KEY"]
 
-#if openai_api_key == "abc":
-#    result = "Nessuna chiave OpenAI disponibile"
-#else:
-#    result = crew.kickoff(inputs={"topic": argomento, "class": classe})
+openai_api_key = "abc"
 
-#st.write(result)
+
+
+
 
 STEM_expert = Agent(
     role="Pianificatore di lezioni di matematica",
@@ -61,15 +58,14 @@ crew = Crew(
 )
 
 if st.button("Pianifica Lezione"):
-    with st.spinner("Pianificazione in corso..."):
-        result = crew.kickoff(inputs={"topic": argomento, "class": classe})
-        st.write(result)
-        
-        #lines = result.raw.splitlines()
-        #lines = lines[2:-2]
-        #result_cleaned = "\n".join(lines)
-        #st.success("Pianificazione completata!")
-        #st.markdown(result_cleaned, unsafe_allow_html=True)
+
+    if openai_api_key == "abc":
+        st.write("Spiacente, attualmente il progetto è in fase di ampliamento e non è operativo.")
+    else:
+        with st.spinner("Pianificazione in corso..."):
+            result = crew.kickoff(inputs={"topic": argomento, "class": classe})
+            st.write(result)
+
 
 
 
