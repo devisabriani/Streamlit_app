4143a547cf745ded525a132b8c9c0a280c9067ff
devisabriani <40249362+devisabriani@users.noreply.github.com>
2024-09-09 23:51:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,13 @@ import streamlit as st
 from crewai import Agent, Task, Crew
 from dotenv import load_dotenv
 load_dotenv()
+
+st.title("EduCrew")
+st.markdown("*by Devis Abriani*")
+
+classe = st.text_input("Inserisci la classe:")
+argomento = st.text_input("Inserisci l'argomento della lezione:")
+
+openai_api_key = st.secrets["OPENAI_API_KEY"]
+
+#openai_api_key = "abc"
