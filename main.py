dc2088c54b772f33c2ce41a870057685ec049534
devisabriani <40249362+devisabriani@users.noreply.github.com>
2024-09-10 00:05:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
-import os
+#import os
 from crewai import Agent, Task, Crew
-from langchain.llms import OpenAI
+#from langchain.llms import OpenAI
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -12,9 +12,9 @@ classe = st.text_input("Inserisci la classe:")
 argomento = st.text_input("Inserisci l'argomento della lezione:")
 
 openai_api_key = st.secrets["OPENAI_API_KEY"]
-os.environ["OPENAI_API_KEY"] = openai_api_key
+#os.environ["OPENAI_API_KEY"] = openai_api_key
 
-llm = OpenAI(temperature=0.7)
+#llm = OpenAI(temperature=0.7)
 
 #openai_api_key = "abc"
 
