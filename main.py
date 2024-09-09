f017208cab1fd622b442cf4ded2e4844bbb32803
devisabriani <40249362+devisabriani@users.noreply.github.com>
2024-09-10 01:51:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 load_dotenv()
 import os
 import streamlit as st
-#from crewai_tools import FileReadTool
+from crewai_tools import FileReadTool
 
 st.title("EduCrew")
 st.markdown("*by Devis Abriani*")
