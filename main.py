a29ff1609151e0fcd15a64be2edfd06f7961dec1
devisabriani <40249362+devisabriani@users.noreply.github.com>
2024-09-10 00:29:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import streamlit as st
-import crewai
-from crewai_tools import *
+from crewai import Agent, Task, Crew
 
 st.title("EduCrew")
 st.markdown("*by Devis Abriani*")
@@ -9,10 +8,16 @@ classe = st.text_input("Inserisci la classe:")
 argomento = st.text_input("Inserisci l'argomento della lezione:")
 
 openai_api_key = st.secrets["OPENAI_API_KEY"]
-#os.environ["OPENAI_API_KEY"] = openai_api_key
-
-#llm = OpenAI(temperature=0.7)
 
 #openai_api_key = "abc"
 
 st.write("")
+
+STEM_expert = Agent(
+    role="Pianificatore di lezioni di matematica",
+    goal="Pianificare lezioni di matematica interessanti per una {class} di un liceo scientifico italiano riguardo {topic}",
+    backstory="Sei un esperto in didattica STEM per le scuole italiane."
+                "Conosci molto bene le connessioni fra la fisica e la matematica insegnate al liceo.",
+    allow_delegation=False,
+  	verbose=True
+)
