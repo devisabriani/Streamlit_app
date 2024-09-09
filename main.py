c8fb3d39c6c0bb1d6ed80a542749d0f25b9213eb
devisabriani <40249362+devisabriani@users.noreply.github.com>
2024-09-10 00:21:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import streamlit as st
-#import os
 from crewai import Agent, Task, Crew
 from crewai_tools import *
-#from langchain.llms import OpenAI
-from dotenv import load_dotenv
-load_dotenv()
 
 st.title("EduCrew")
 st.markdown("*by Devis Abriani*")
@@ -19,8 +15,4 @@ openai_api_key = st.secrets["OPENAI_API_KEY"]
 
 #openai_api_key = "abc"
 
-wow = Agent(
-  role="Batman",
-  goal="Sconfiggere il male",
-  backstory="Sono Batman!"
-)
+st.write("Ciao")
