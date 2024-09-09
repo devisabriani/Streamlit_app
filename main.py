1b317b32000bd0595f94e23791156dcfb2a3ac5f
devisabriani <40249362+devisabriani@users.noreply.github.com>
2024-09-09 23:35:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ st.markdown("*by Devis Abriani*")
 classe = st.text_input("Inserisci la classe:")
 argomento = st.text_input("Inserisci l'argomento della lezione:")
 
-#openai_api_key = st.secrets["OPENAI_API_KEY"]
+openai_api_key = st.secrets["OPENAI_API_KEY"]
 
-openai_api_key = "abc"
+#openai_api_key = "abc"
 
 
 
