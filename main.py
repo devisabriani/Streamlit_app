c6f8e7ca6bdff107198d810e9620a891b64f90f2
devisabriani <40249362+devisabriani@users.noreply.github.com>
2024-09-09 23:53:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,4 +13,19 @@ openai_api_key = st.secrets["OPENAI_API_KEY"]
 
 #openai_api_key = "abc"
 
+STEM_expert = Agent(
+    role="Pianificatore di lezioni di matematica",
+    goal="Pianificare lezioni di matematica interessanti per una {class} di un liceo scientifico italiano riguardo {topic}",
+    backstory="Sei un esperto in didattica STEM per le scuole italiane."
+                "Conosci molto bene le connessioni fra la fisica e la matematica insegnate al liceo.",
+    allow_delegation=False,
+  	verbose=True
+)
 
+DigComp_expert = Agent(
+    role="Esperto del framework europeo DigComp",
+    goal="Revisionare la pianificazione dello STEM_expert in modo da aggiungere elementi dal DigComp.",
+    backstory="Sei un esperto di DigComp e DigCompEdu.",
+    allow_delegation=False,
+    verbose=True
+)
