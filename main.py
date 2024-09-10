ce1d96bf24e62298f09b40d6899bb10e24017c35
devisabriani <40249362+devisabriani@users.noreply.github.com>
2024-09-11 00:22:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,9 +11,9 @@ st.markdown("*by Devis Abriani*")
 classe = st.text_input("Inserisci la classe:")
 argomento = st.text_input("Inserisci l'argomento della lezione:")
 
-openai_api_key = st.secrets["OPENAI_API_KEY"]
+#openai_api_key = st.secrets["OPENAI_API_KEY"]
 
-#openai_api_key = "abc"
+openai_api_key = "abc"
 
 #st.write("Sito web in fase di ampliamento. Non operativo.")
 
